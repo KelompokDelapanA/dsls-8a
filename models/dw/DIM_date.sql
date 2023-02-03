c7b3721b0cb5894df269a34f72eef672a0e01dd9
dsls.kelompok.8a@gmail.com <dsls.kelompok.8a@gmail.com>
2023-02-03 08:32:39 +0000

    dim and fact for DBR data rev 1

--- models/dw/DIM_date.sql
+++ models/dw/DIM_date.sql
@@ -2,7 +2,7 @@
 
 WITH date_ AS (
         SELECT *
-        FROM UNNEST(GENERATE_DATE_ARRAY('2021-01-01', '2022-02-01', INTERVAL 1 DAY)) AS `DATE`
+        FROM UNNEST(GENERATE_DATE_ARRAY('2021-01-01', '2023-02-01', INTERVAL 1 DAY)) AS `DATE`
 ),
 date_id_ AS (
     SELECT row_number() over(ORDER BY `DATE` ASC) AS date_id,
--- models/dw/DIM_sub_comp.sql
+++ models/dw/DIM_sub_comp.sql
@@ -1,12 +1,13 @@
 {{ config(materialized="table") }}
 
 with
-    sub_comps as (select SUB_COMP_CODE, DESCRIPTION_SUB_COMP__PA_ as DESC_SUB_COMP_PA , COMP_CODE from `raw_data.raw_all_data_dbr` 
-            where DESCRIPTION_SUB_COMP__PA_ IS NOT NULL
-            group by 1,2,3),
-    join_comps as (select SUB_COMP_CODE, DESC_SUB_COMP_PA, comp.COMP_CODE, comp.DESC_COMP_PA
+    sub_comps as (select SUB_COMP_CODE, TRIM(UPPER(DESCRIPTION_SUB_COMP__PA_)) as DESC_SUB_COMP , 
+                    COMP_CODE 
+                    from `raw_data.raw_all_data_dbr` 
+                    where DESCRIPTION_SUB_COMP__PA_ IS NOT NULL
+                    group by 1,2,3),
+    join_comps as (select SUB_COMP_CODE, DESC_SUB_COMP, comp.COMP_CODE, comp.DESC_COMP
                    FROM sub_comps sc
                    LEFT JOIN {{ ref('DIM_comp')}} comp ON comp.COMP_CODE = sc.COMP_CODE)
-
 select *
 from join_comps
